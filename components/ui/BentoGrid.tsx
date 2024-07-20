952c562919cc7a1fdbb24956d9937a059f47f3cf
Marcott <kasangandjomarcott47@gmail.com>
2024-07-20 19:29:25 +0200

    Grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -53,7 +53,6 @@ export const BentoGridItem = ({
         }
       }
     >
-    
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">

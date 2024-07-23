8b3a5374b43da7ed456f2e8a9330261f576e2d42
Marcott <kasangandjomarcott47@gmail.com>
2024-07-23 09:07:31 +0200

    Testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
 import RecentProject from "@/components/RecentProject";
 import { navItems } from "@/data";
+import Clients from "@/components/Clients";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProject />
+        <Clients />
       </div>
     </main>
   );
---
+++ components/Clients.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { InfiniteMovingCards } from './ui/InfinitteMovingCards'
+import { testimonials } from '@/data'
+
+function Clients() {
+    return (
+        <div className='py-20' id="projects">
+            <h1 className="heading">
+                Témoignages de clients{' '}
+                <span className='text-purple'> satisfaits
+                </span>
+            </h1>
+            <div className='flex flex-col items-center'>
+                <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden">
+                    <InfiniteMovingCards
+                        items={testimonials}
+                        direction='right'
+                        speed='slow'
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Clients
